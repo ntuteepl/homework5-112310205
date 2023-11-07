0570a347fa7cd105d8b2b47e1182dc983e927b10
112310205 <146065065+112310205@users.noreply.github.com>
2023-11-07 12:25:37 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double h,m,a,b,c,t;
+    scanf("%lf %lf",&h,&m);
+    if(h>=0&&m>=0)
+        {
+    a=30*h+0.5*m;
+    b=m*6;
+
+    if(a<b)
+        {
+            t=a;
+            a=b;
+            b=t;
+        }
+    c=a-b;
+    if(c>180)
+        {
+        c=360-c;
+        }
+    printf("%.3f\n",c);
+        }
+}
 
